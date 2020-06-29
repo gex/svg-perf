b0da96c91e12b6de9ea04f8a2f11f9356dd84184
Gergo Szonyi <github@notgex.dev>
2020-06-29 05:17:15 +0200

    add link to the html version
    - add missing dev dependency

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Layout from '../components/Layout'
 const Index = () => (
   <Layout>
     <h1>SVG Perf</h1>
+    <p>
+      <a href="https://gex.github.io/html-perf/">HTML version</a>
+    </p>
   </Layout>
 )
 
